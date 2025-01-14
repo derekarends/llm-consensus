2c21830531e0bb1f244ba574185f38b4cc1fc532
Derek Arends <derek@thinkovator.com>
2025-01-13 21:44:15 -0600

    update deployments

--- web/app/__init__.py
+++ web/app/__init__.py
@@ -9,6 +9,6 @@ settings = Settings(
     azure_openai_api_version=os.getenv("AZURE_OPENAI_API_VERSION", "2024-05-01-preview"),
     azure_openai_deployment_name=os.getenv("AZURE_OPENAI_DEPLOYMENT_NAME", ""),
     azure_openai_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT", ""),
-    redis_host=os.getenv("REDIS_HOST", "redis.service"),
+    redis_host=os.getenv("REDIS_HOST", "redis-service"),
     redis_port=int(os.getenv("REDIS_PORT", 6379)),
 )
